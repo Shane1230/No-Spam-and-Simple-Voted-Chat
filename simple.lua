a1e5b231346bbae436e1ed4c5cbe652a05eae339
Shane1230 <dddiibbb@naver.com>
2023-07-24 12:12:58 +0900

    2.0
    U238.3

--- simple.lua
+++ simple.lua
@@ -1,5 +1,7 @@
-Hooks:Add("LocalizationManagerPostInit", "menu", function(loc)
-	LocalizationManager:add_localized_strings({
-        menu_chat_preplanning_vote_on_plan = "$name; | $type;" --$name voted on the $plan: $type
-	})
-end)
+if NSSVC.Options.simple_chat == true then
+	Hooks:Add("LocalizationManagerPostInit", "vote", function(loc)
+		LocalizationManager:add_localized_strings({
+			menu_chat_preplanning_vote_on_plan = "$name; | $type;" -- $name voted on the $plan: $type
+		})
+	end)
+end
